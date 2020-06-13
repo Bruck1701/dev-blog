1ce444efb1debcc6496e257af3f26a4fa2dd2732
Bruck1701 <kummel@gmail.com>
2020-06-13 23:16:06 +0200

    feat: refactor front end

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,10 @@ class Article < ApplicationRecord
   belongs_to :category
   # validations of field: 
   validates :title, presence: true, length: {minimum:6, maximum:100}
-  validates :content, presence: true, length: {minimum:10, maximum: 300 }
+  validates :content, presence: true, length: {minimum:10, maximum: 2500 }
+
+  def title=(s)
+    super s.capitalize
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   
   root "pages#home"
+  get "new", :to => "articles#new"
   get "about" , :to => "pages#about"
   resources :articles #, only: [:show, :index, :new, :create, :edit, :update]
 
